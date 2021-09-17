05bf0b500466978dd3e074f8c151fc4c39f3767a
Raymond <raymond@philippinedev.com>
2021-09-17 12:17:58 +0800

    refactor change matcher

--- section_3/change_matcher_spec.rb
+++ section_3/change_matcher_spec.rb
@@ -1,5 +1,4 @@
 RSpec.describe 'change matcher' do
-<<<<<<< HEAD
   subject { [5, 10, 15] }
 
   it 'checks that a method changes object state' do
@@ -12,19 +11,5 @@ RSpec.describe 'change matcher' do
 
   it 'has not changed' do
     expect { subject.count }.to change { subject.count }.by 0
-=======
-  subject(:num) { [5, 10, 15] }
-
-  it 'checks that a method changes object state' do
-    expect { num.push(20) }.to change { subject.count }.by 1
-  end
-
-  it 'accepts negative arguments' do
-    expect { num.pop }.to change { subject.count }.by -1
-  end
-
-  it 'has not changed' do
-    expect { num.count }.to change { subject.count }.by 0
->>>>>>> 713da3a8f4b83acdcb7ddd28501d12e7178c86f2
   end
 end
